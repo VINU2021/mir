52e359cb98b3ca1d9b2f6bac137b0f04b11b87a4
VINU2021 <vinushrees21@gmail.com>
2023-04-08 13:40:47 +0530

    part 6

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: 'cello ball point pen',
+      slug: 'cello-ball-pen',
+      category: 'pen',
+      image: '/image/p1.png', //679x829
+      price: 120,
+      brand: 'cello',
+      rating: 4.2,
+      numReviews: 10,
+      description: 'high quality',
+    },
+    {
+      name: 'cello ball point pen',
+      slug: 'cello-ball-pen',
+      category: 'pen',
+      image: '/image/p1.png',
+      price: 120,
+      brand: 'cello',
+      rating: 4.2,
+      numReviews: 10,
+      description: 'high quality',
+    },
+    {
+      name: 'cello ball point pen',
+      slug: 'cello-ball-pen',
+      category: 'pen',
+      image: '/image/p1.png',
+      price: 120,
+      brand: 'cello',
+      rating: 4.2,
+      numReviews: 10,
+      description: 'high quality',
+    },
+  ],
+};
+export default data;
